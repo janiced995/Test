34bca6e99f03455ab6d4bb8fadc3e0601fcddba2
Janice David <janiced995@gmail.com>
2020-01-25 10:09:24 -0500

    update

--- FristTest/src/main/java/com/selenium/FirstTest/LoginTest.java
+++ FristTest/src/main/java/com/selenium/FirstTest/LoginTest.java
@@ -4,12 +4,11 @@ public class LoginTest {
 	
 	public static void login() {
 		System.out.println("login");
-		System.out.println("login");
 		
 	}
 	
 	public static void HomePage() {
-		System.out.println("Home Page");
+		System.out.println("Home Page first");
 		System.out.println("Home Page");
 	}
 
